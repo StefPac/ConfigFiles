ab0031ab4cb8892a5fd73cc7b02bfa671d20591f
Stefano Pacifico <stefano.pacifico@ieee.org>
2012-11-26 22:43:57 -0500

    Enabled spacific-rcirc, and minor embellishment.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,8 +1,17 @@
 (add-to-list 'load-path (expand-file-name "~/.emacs.d"))
 
+;; generic
 (require 'spacific-base)
-(require 'spacific-looknfeel)
-(require 'spacific-git)
-(require 'spacific-org)
 
+;; eye-candy
+(require 'spacific-looknfeel)
+
+;; git
+(require 'spacific-git)
+
+;; org-mode
+(require 'spacific-org) 
+
+;; IRC settings
+(require 'spacific-rcirc) 
 
