a5db2d5705801585835be8d219306c01b01e9213
Stefano Pacifico <github@pacifico.cc>
2021-05-02 16:41:29 -0400

    Start emacs server if not present

--- .emacs.d/elisp/spacific-server.el
+++ .emacs.d/elisp/spacific-server.el
@@ -1,8 +1,13 @@
 ;; define function to shutdown emacs server instance
+(require 'server)
+
 (defun server-shutdown ()
   "Save buffers, Quit, and Shutdown (kill) server"
   (interactive)
   (save-some-buffers)
     (kill-emacs))
+
+(unless (server-running-p)
+  (server-start))
 
 (provide 'spacific-server)
