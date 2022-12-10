c94eb8604f6cb6aa16bf749c4a07698545543a24
Stefano Pacifico <github@pacifico.cc>
2022-12-10 14:48:32 -0500

    Simplified Emacs look and feel

--- .emacs.d/elisp/spacific-looknfeel.el
+++ .emacs.d/elisp/spacific-looknfeel.el
@@ -19,23 +19,23 @@
 
 ;;(set-face-attribute 'default nil :background "brightwhite")
 
-;;(set-face-background 'indent-guide-face "dimgray")
+;; (set-face-background 'indent-guide-face "black")
 
-;; Global settings (defaults)
+;; ;; Global settings (defaults)
 (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-    doom-themes-enable-italic t) ; if nil, italics is universally disabled
+      doom-themes-enable-italic t) ; if nil, italics is universally disabled
 
-;; Load the theme (doom-one, doom-molokai, etc); keep in mind that each
-;; theme may have their own settings.
-(load-theme 'doom-vibrant t)
+;; ;; Load the theme (doom-one, doom-molokai, etc); keep in mind that each
+;; ;; theme may have their own settings.
+(load-theme 'github-dark-vscode t)
 
-;; Enable flashing mode-line on errors
+;; ;; Enable flashing mode-line on errors
 (doom-themes-visual-bell-config)
 
-;; Enable custom neotree theme
-(doom-themes-neotree-config)  ; all-the-icons fonts must be installed!
+;; ;; Enable custom neotree theme
+;; (doom-themes-neotree-config)  ; all-the-icons fonts must be installed!
 
-(require 'doom-modeline)
-(doom-modeline-mode 1)
+;; (require 'doom-modeline)
+;; (doom-modeline-mode 1)
 
 (provide 'spacific-looknfeel)
