ca827a3a14bd8dda959b09b365b08955ea192b70
Stefano Pacifico <github@pacifico.cc>
2023-09-13 16:02:56 -0400

    fix typo in .gitignore, emacs: enable eglot

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -46,10 +46,10 @@
 (require 'spacific-helm)
 
 ;; Set LSP for languages
-(require 'spacific-lsp)
+;; (require 'spacific-lsp)
 
 ;; Set eglot
-;; (require 'spacific-eglot)
+(require 'spacific-eglot)
 
 ;; Load Rust settings
 (require 'spacific-rust)
