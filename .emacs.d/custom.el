3920656bbaaa64897e9f81497273e88cb570731b
Stefano Pacifico <github@pacifico.cc>
2023-11-24 02:59:31 +0000

    emacs: update custom.el

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -4,28 +4,24 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(yasnippet-snippets wfnames web-completion-data vertico undo-tree
-                        tuareg tree-sitter toc-org rustic ripgrep rg
-                        quelpa-use-package python-black pyenv-mode
-                        projectile pos-tip popup perspective ox-rst
-                        ox-gfm orderless olivetti ob-ipython neotree
-                        marginalia magithub magit-reviewboard
-                        magit-gh-pulls magit-diff-flycheck lsp-ui
-                        lsp-pyright key-chord js2-refactor
-                        indent-guide github-review github-modern-theme
-                        github-dark-vscode-theme evil-visualstar
-                        evil-terminal-cursor-changer evil-tabs
-                        evil-surround evil-space
-                        evil-search-highlight-persist evil-paredit
-                        evil-org evil-numbers evil-nerd-commenter
-                        evil-matchit evil-lisp-state evil-leader
-                        evil-god-state evil-exchange evil-escape
-                        evil-commentary evil-args embark-consult
-                        emacsql-sqlite elpy eglot doom-themes
-                        doom-modeline deadgrep corfu-terminal copilot
-                        code-review cider bibtex-completion bazel
-                        auctex async apheleia all-the-icons-dired
-                        ac-js2)))
+   '(csv-mode yasnippet-snippets wfnames web-completion-data vertico
+              undo-tree tuareg tree-sitter toc-org rustic ripgrep rg
+              quelpa-use-package python-black pyenv-mode projectile
+              pos-tip popup perspective ox-rst ox-gfm orderless
+              olivetti ob-ipython neotree marginalia magithub
+              magit-reviewboard magit-gh-pulls magit-diff-flycheck
+              lsp-ui lsp-pyright key-chord js2-refactor indent-guide
+              github-review github-modern-theme
+              github-dark-vscode-theme evil-visualstar
+              evil-terminal-cursor-changer evil-tabs evil-surround
+              evil-space evil-search-highlight-persist evil-paredit
+              evil-org evil-numbers evil-nerd-commenter evil-matchit
+              evil-lisp-state evil-leader evil-god-state evil-exchange
+              evil-escape evil-commentary evil-args embark-consult
+              emacsql-sqlite elpy eglot doom-themes doom-modeline
+              deadgrep corfu-terminal copilot code-review cider
+              bibtex-completion bazel auctex async apheleia
+              all-the-icons-dired ac-js2)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
