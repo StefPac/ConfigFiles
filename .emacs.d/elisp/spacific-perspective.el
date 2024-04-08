967964d2cc9540ff01b868e32d55e0584e889f7f
Stefano Pacifico <github@pacifico.cc>
2024-04-08 12:54:48 -0400

    emacs: fix syntax and add persp buffers to consult

--- .emacs.d/elisp/spacific-perspective.el
+++ .emacs.d/elisp/spacific-perspective.el
@@ -1,5 +1,5 @@
 (use-package perspective
- 
+  
   :bind
   ("C-x C-b" . persp-list-buffers)         ; or use a nicer switcher, see below
   ("C-x C-p" . persp-switch)
@@ -10,6 +10,10 @@
   (global-set-key (kbd "C-x C-c") 'spacific/save-buffers-persp-kill-terminal)
   (setq persp-state-default-file (concat user-emacs-directory "persp-auto-save"))
   (persp-mode)
-  (persp-state-load persp-state-default-file))
+  (persp-state-load persp-state-default-file)
+  ;; if consult is loaded
+  (with-eval-after-load 'consult)
+  (add-to-list 'consult-buffer-sources persp-consult-source))
+
 
 (provide 'spacific-perspective)
