966221133c15e8bf324751c079884b1330e76bc3
Stefano Pacifico <github@pacifico.cc>
2024-05-04 16:56:23 -0400

    emacs: add denote package, disable ox-bibtex and reformat spacific-org.el

--- .emacs.d/elisp/spacific-org.el
+++ .emacs.d/elisp/spacific-org.el
@@ -20,9 +20,9 @@
 (org-babel-do-load-languages
  'org-babel-load-languages
  '(
-    (ditaa . t) ; this line activat
-    (shell . t)
-))
+   (ditaa . t) ; this line activat
+   (shell . t)
+   ))
 
 ;; Without this `fill-paragrah` does not work in `org-mode`
 (setq org-list-allow-alphabetical t)
@@ -63,47 +63,47 @@
 
 
 (defun my-auto-tex-parameters (backend-symbol)
-       ;; "Automatically select the tex packages to include."
-       ;; default packages for ordinary latex or pdflatex export
+  ;; "Automatically select the tex packages to include."
+  ;; default packages for ordinary latex or pdflatex export
   (setq org-latex-default-packages-alist
-     '(("AUTO" "inputenc" t)
-       ("T1"   "fontenc"   t)
-       (""     "fixltx2e"  nil)
-       (""     "wrapfig"   nil)
-       (""     "soul"      t)
-       (""     "textcomp"  t)
-       (""     "marvosym"  t)
-       (""     "wasysym"   t)
-       (""     "latexsym"  t)
-       (""     "amssymb"   t)
-       (""     "hyperref"  nil)))
+        '(("AUTO" "inputenc" t)
+          ("T1"   "fontenc"   t)
+          (""     "fixltx2e"  nil)
+          (""     "wrapfig"   nil)
+          (""     "soul"      t)
+          (""     "textcomp"  t)
+          (""     "marvosym"  t)
+          (""     "wasysym"   t)
+          (""     "latexsym"  t)
+          (""     "amssymb"   t)
+          (""     "hyperref"  nil)))
 
   ;; Packages to include when xelatex is used
   (if (string-match "LATEX_CMD: xelatex" (buffer-string))
       (setq org-latex-default-packages-alist
-	    '(("" "xunicode" t)
-	      ("" "url" t)
-	      ("" "rotating" t)
-	      ("american" "babel" t)
-	      ("babel" "csquotes" t)
-	      ("" "soul" t)
-	      ("" "float" nil)
+	        '(("" "xunicode" t)
+	          ("" "url" t)
+	          ("" "rotating" t)
+	          ("american" "babel" t)
+	          ("babel" "csquotes" t)
+	          ("" "soul" t)
+	          ("" "float" nil)
               ("" "longtable" nil)
-	      ("xetex" "graphicx" nil)
-	      ("xetex" "hyperref" nil)
-	      )))
+	          ("xetex" "graphicx" nil)
+	          ("xetex" "hyperref" nil)
+	          )))
 
   (if (string-match "LATEX_CMD: xelatex" (buffer-string))
       (setq org-latex-classes
-	    (cons '("article"
-		    "\\documentclass[11pt,article,oneside]{memoir}"
-		    ("\\section{%s}" . "\\section*{%s}")
-		    ("\\subsection{%s}" . "\\subsection*{%s}")
-		    ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
-		    ("\\paragraph{%s}" . "\\paragraph*{%s}")
-		    ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))
-		  org-latex-classes)))
-)
+	        (cons '("article"
+		            "\\documentclass[11pt,article,oneside]{memoir}"
+		            ("\\section{%s}" . "\\section*{%s}")
+		            ("\\subsection{%s}" . "\\subsection*{%s}")
+		            ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+		            ("\\paragraph{%s}" . "\\paragraph*{%s}")
+		            ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))
+		          org-latex-classes)))
+  )
 
 (add-hook 'org-export-before-processing-hook 'my-auto-tex-parameters)
 
@@ -116,7 +116,7 @@
 ;; (setq org-latex-listings 'minted)
 ;; (add-to-list 'org-latex-packages-alist '("" "minted"))
 ;; (setq org-latex-to-pdf-process
-      ;; '("pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f" "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f" "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"))
+;; '("pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f" "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f" "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"))
 
 ;; Add 'letter' document class
 (add-to-list 'org-latex-classes
@@ -130,32 +130,33 @@
              )
 (defun org-mode-reftex-setup ()
   (load-library "reftex")
-  (require 'ox-bibtex)
+  ;; TODO: fix ox-bibtex configuration
+  ;; (require 'ox-bibtex)
   (and (buffer-file-name) (file-exists-p (buffer-file-name))
        (progn
-	;enable auto-revert-mode to update reftex when bibtex file changes on disk
-	 (global-auto-revert-mode t)
-	 (reftex-parse-all)
-	;add a custom reftex cite format to insert links
-	 (reftex-set-cite-format
-	  '((?b . "[[bib:%l][%l-bib]]")
-	    (?n . "[[notes:%l][%l-notes]]")
-	    (?p . "[[papers:%l][%l-paper]]")
-	    (?c . "[[cite:%l][%l-cite]]")
-	    (?i . "[[citep:%l][%l-citep]]")
-	    (?t . "[[citet:%l][%l-citet]]")
-	    (?l . "%t")
-	    (?h . "** %t\n:PROPERTIES:\n:Custom_ID: %l\n:END:\n[[papers:%l][%l-paper]]")))))
+                                        ;enable auto-revert-mode to update reftex when bibtex file changes on disk
+	     (global-auto-revert-mode t)
+	     (reftex-parse-all)
+                                        ;add a custom reftex cite format to insert links
+	     (reftex-set-cite-format
+	      '((?b . "[[bib:%l][%l-bib]]")
+	        (?n . "[[notes:%l][%l-notes]]")
+	        (?p . "[[papers:%l][%l-paper]]")
+	        (?c . "[[cite:%l][%l-cite]]")
+	        (?i . "[[citep:%l][%l-citep]]")
+	        (?t . "[[citet:%l][%l-citet]]")
+	        (?l . "%t")
+	        (?h . "** %t\n:PROPERTIES:\n:Custom_ID: %l\n:END:\n[[papers:%l][%l-paper]]")))))
   (define-key org-mode-map (kbd "C-c )") 'reftex-citation)
   (define-key org-mode-map (kbd "C-c (") 'org-mode-reftex-search))
-  (define-key org-mode-map (kbd "C-c x b") 'org-narrow-to-block)
+(define-key org-mode-map (kbd "C-c x b") 'org-narrow-to-block)
 
 (add-hook 'org-mode-hook 'org-mode-reftex-setup)
 
 (defun org-mode-reftex-search ()
   ;;jump to the notes for the paper pointed to at from reftex search
   (interactive)
-    (org-open-link-from-string (format "[[notes:%s]]" (first (reftex-citation t)))))
+  (org-open-link-from-string (format "[[notes:%s]]" (first (reftex-citation t)))))
 
 (if (require 'toc-org nil t)
     (progn
@@ -164,6 +165,15 @@
       ;; TODO enable in markdown, too
       ;; (add-hook 'markdown-mode-hook 'toc-org-mode)
       ;; (define-key markdown-mode-map (kbd "\C-c\C-o") 'toc-org-markdown-follow-thing-at-point))
-  (warn "toc-org not found"))
-)
+      (warn "toc-org not found"))
+  )
+
+;; setup denote
+(use-package denote
+  :ensure t
+  :config
+  (setq denote-extensions '(("org" . org-mode)))
+  )
+
+
 (provide 'spacific-org)
